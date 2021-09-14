7e592b3f59bdc5f8540d1469be7fb0b5822f0c50
rafik mrabet <mrabetr@hotmail.com>
2021-09-14 12:05:24 +0100

    updated current_resource_owner method to Customer

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,7 +4,8 @@ module Api::V1
   # class ApiController < ::ApplicationController
   class BaseController < ActionController::API
     def current_resource_owner
-      User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+      # User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+      Customer.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
     end
 
     def current_application
